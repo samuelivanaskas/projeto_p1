06a0d79c9b9b5d35b451453d6833a50c7834ee07
samuelivanaskas <106975862+samuelivanaskas@users.noreply.github.com>
2023-05-19 14:56:38 -0300

    Update styles.ts

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,34 +2,22 @@ import styled from "styled-components";
 import { cores } from "../../styles";
 
 export const HeaderBar = styled.header`
-  position: absolute;
-  width: 2031.81px;
-  height: 384px;
-  left: 0px;
-  top: -24px;
   background-color: ${cores.rosaclaro};
+  padding: 24px;
+  border-radius: 16px;
+  margin-bottom: 80px;
+  display: flex;
+  align-items: center;
 `;
 
 export const Ilogo = styled.image`
   position: absolute;
-  width: 125px;
-  height: 57.5px;
-  left: 621px;
-  top: 40px;
   background-color: ${cores.rosaclaro};
 `;
 
 export const Text = styled.p`
-  position: absolute;
-  width: 539px;
-  height: 84px;
-  left: 414px;
-  top: 236px;
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 900;
-  font-size: 36px;
-  line-height: 42px;
-  text-align: center;
+  position: relative;
+  font-size: 32px;
+  margin-left: 8px;
   color: ${cores.rosa};
 `;
